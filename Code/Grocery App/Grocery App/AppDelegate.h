e6a342aa5c1517377c2b9a374eca096c4556a58b
Rishabh Tayal <rtayal11@gmail.com>
2014-09-24 12:57:38 -0500

    commit.

---
+++ Code/Grocery App/Grocery App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Grocery App
+//
+//  Created by Rishabh Tayal on 9/24/14.
+//  Copyright (c) 2014 Appikon Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code/Grocery App/Grocery App/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Grocery App
+//
+//  Created by Rishabh Tayal on 9/24/14.
+//  Copyright (c) 2014 Appikon Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
