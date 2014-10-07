5f1a4d8a6c12ed05e4d6fe33a78c33870b8d73fc
Rishabh Tayal <rtayal11@gmail.com>
2014-10-07 11:29:30 -0500

    commit.

--- Code/Grocery App/GRCheckoutViewController.m
+++ Code/Grocery App/GRCheckoutViewController.m
@@ -10,9 +10,12 @@
 #import <AddressBook/AddressBook.h>
 #import <AddressBookUI/AddressBookUI.h>
 
-@interface GRCheckoutViewController ()<ABPeoplePickerNavigationControllerDelegate>
+@interface GRCheckoutViewController ()<ABPeoplePickerNavigationControllerDelegate, UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UITableView* tableView;
 
 @property (nonatomic, strong) ABPeoplePickerNavigationController *addressBookController;
+@property (nonatomic, strong) NSMutableArray* arrContactsData;
 
 -(IBAction)importFromContactsClicked:(id)sender;
 
@@ -45,6 +48,126 @@
     [_addressBookController setPeoplePickerDelegate:self];
     [self presentViewController:_addressBookController animated:YES completion:nil];
 
+}
+
+#pragma mark - UITableView Datasource
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _arrContactsData.count;
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.textLabel.text = @"A";
+    return cell;
+}
+
+#pragma mark - ABPeoplePickerNavigationController Delegate method implementation
+
+-(void)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker didSelectPerson:(ABRecordRef)person
+{
+    // Initialize a mutable dictionary and give it initial values.
+    NSMutableDictionary *contactInfoDict = [[NSMutableDictionary alloc]
+                                            initWithObjects:@[@"", @"", @"", @"", @"", @"", @"", @"", @""]
+                                            forKeys:@[@"firstName", @"lastName", @"mobileNumber", @"homeNumber", @"homeEmail", @"workEmail", @"address", @"zipCode", @"city"]];
+    
+    // Use a general Core Foundation object.
+    CFTypeRef generalCFObject = ABRecordCopyValue(person, kABPersonFirstNameProperty);
+    
+    // Get the first name.
+    if (generalCFObject) {
+        [contactInfoDict setObject:(__bridge NSString *)generalCFObject forKey:@"firstName"];
+        CFRelease(generalCFObject);
+    }
+    
+    // Get the last name.
+    generalCFObject = ABRecordCopyValue(person, kABPersonLastNameProperty);
+    if (generalCFObject) {
+        [contactInfoDict setObject:(__bridge NSString *)generalCFObject forKey:@"lastName"];
+        CFRelease(generalCFObject);
+    }
+    
+    // Get the phone numbers as a multi-value property.
+    ABMultiValueRef phonesRef = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    for (int i=0; i<ABMultiValueGetCount(phonesRef); i++) {
+        CFStringRef currentPhoneLabel = ABMultiValueCopyLabelAtIndex(phonesRef, i);
+        CFStringRef currentPhoneValue = ABMultiValueCopyValueAtIndex(phonesRef, i);
+        
+        if (CFStringCompare(currentPhoneLabel, kABPersonPhoneMobileLabel, 0) == kCFCompareEqualTo) {
+            [contactInfoDict setObject:(__bridge NSString *)currentPhoneValue forKey:@"mobileNumber"];
+        }
+        
+        if (CFStringCompare(currentPhoneLabel, kABHomeLabel, 0) == kCFCompareEqualTo) {
+            [contactInfoDict setObject:(__bridge NSString *)currentPhoneValue forKey:@"homeNumber"];
+        }
+        
+        CFRelease(currentPhoneLabel);
+        CFRelease(currentPhoneValue);
+        CFRelease(phonesRef);
+    }
+    
+    
+    // Get the e-mail addresses as a multi-value property.
+    ABMultiValueRef emailsRef = ABRecordCopyValue(person, kABPersonEmailProperty);
+    for (int i=0; i<ABMultiValueGetCount(emailsRef); i++) {
+        CFStringRef currentEmailLabel = ABMultiValueCopyLabelAtIndex(emailsRef, i);
+        CFStringRef currentEmailValue = ABMultiValueCopyValueAtIndex(emailsRef, i);
+        
+        if (CFStringCompare(currentEmailLabel, kABHomeLabel, 0) == kCFCompareEqualTo) {
+            [contactInfoDict setObject:(__bridge NSString *)currentEmailValue forKey:@"homeEmail"];
+        }
+        
+        if (CFStringCompare(currentEmailLabel, kABWorkLabel, 0) == kCFCompareEqualTo) {
+            [contactInfoDict setObject:(__bridge NSString *)currentEmailValue forKey:@"workEmail"];
+        }
+        
+        CFRelease(currentEmailLabel);
+        CFRelease(currentEmailValue);
+        CFRelease(emailsRef);
+    }
+    
+    
+    
+    // Get the first street address among all addresses of the selected contact.
+    ABMultiValueRef addressRef = ABRecordCopyValue(person, kABPersonAddressProperty);
+    if (ABMultiValueGetCount(addressRef) > 0) {
+        NSDictionary *addressDict = (__bridge NSDictionary *)ABMultiValueCopyValueAtIndex(addressRef, 0);
+        
+        [contactInfoDict setObject:[addressDict objectForKey:(NSString *)kABPersonAddressStreetKey] forKey:@"address"];
+        [contactInfoDict setObject:[addressDict objectForKey:(NSString *)kABPersonAddressZIPKey] forKey:@"zipCode"];
+        [contactInfoDict setObject:[addressDict objectForKey:(NSString *)kABPersonAddressCityKey] forKey:@"city"];
+        CFRelease(addressRef);
+    }
+    
+    
+    // If the contact has an image then get it too.
+    if (ABPersonHasImageData(person)) {
+        NSData *contactImageData = (__bridge NSData *)ABPersonCopyImageDataWithFormat(person, kABPersonImageFormatThumbnail);
+        
+        [contactInfoDict setObject:contactImageData forKey:@"image"];
+    }
+    
+    // Initialize the array if it's not yet initialized.
+    if (_arrContactsData == nil) {
+        _arrContactsData = [[NSMutableArray alloc] init];
+    }
+    // Add the dictionary to the array.
+    [_arrContactsData addObject:contactInfoDict];
+    
+    // Reload the table view data.
+//    [self.tableView reloadData];
+    
+    // Dismiss the address book view controller.
+    [_addressBookController dismissViewControllerAnimated:YES completion:nil];
+    
+    //    return NO;
+}
+
+-(void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker{
+    [_addressBookController dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
